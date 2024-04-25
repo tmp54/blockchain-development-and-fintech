2929b966180c3fa76b639e45e79a4725b9a7be04
tmp54 <80036073+tmp54@users.noreply.github.com>
2024-04-25 18:14:40 +0000

    chore: update .env path in lab2/lab3

--- lab2/hardhat.config.ts
+++ lab2/hardhat.config.ts
@@ -1,7 +1,9 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import 'solidity-docgen'
-import 'dotenv/config'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
--- lab3/hardhat.config.ts
+++ lab3/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
-import 'dotenv/config'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
